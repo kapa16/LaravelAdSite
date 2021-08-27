e78bb1d61c3e7762d44a74138b91152e74b9ce27
kapa16 <kapa16@inbox.ru>
2021-08-27 21:19:20 +0500

    change dir for webpack build js & css. Gitignor public/build

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('public/build')
+    .js('resources/js/app.js', 'js')
+    .postCss('resources/css/app.css', 'css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .version();
